726049dd26a038e471e06b49118114c70ee751d1
Lucas Vilarim <lucasdsvilarim18@gmail.com>
2024-09-18 12:00:59 -0300

    new activities

--- desafio14.py
+++
@@ -1,5 +0,0 @@
-a = 5
-
-for m in range(1, a+1):
-    for x in range(m):
-        print(m, end = "")
--- desafio16.py
+++
@@ -1,5 +0,0 @@
-a = 3
-for i in range(0,a+1):
-    for x in range(i):
-        print(x, end = " ")
-    print(i)
